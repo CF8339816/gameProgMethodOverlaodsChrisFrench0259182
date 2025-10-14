c20e953200ba1dc0c03a07524130e25a2eb2c924
CF8339816 <chris_french_3dga@hotmail.com>
2025-10-14 20:11:48 -0300

    minimums working

--- gameProgMethodOverlaodsChrisFrench0259182/Program.cs
+++ gameProgMethodOverlaodsChrisFrench0259182/Program.cs
@@ -31,8 +31,16 @@ namespace gameProgMethodOverlaodsChrisFrench0259182
             //int roll = random.Next(1, sides + 1 );
 
             SixSide();
+            Console.WriteLine();
+
             randSide();
+            Console.WriteLine();
+
             RandSidesTimes();
+            Console.WriteLine();
+
+            RandRed();
+            Console.WriteLine();
 
 
 
@@ -43,9 +51,9 @@ namespace gameProgMethodOverlaodsChrisFrench0259182
 
         }
 
-       //methods below here 
-       //m1
-       static void  SixSide()
+        //methods below here 
+        //m1
+        static void  SixSide()
         {
             sides = 6;
                       
@@ -63,7 +71,7 @@ namespace gameProgMethodOverlaodsChrisFrench0259182
         static void randSide()
         {
             
-            Console.WriteLine($" how many sides does your Die have?");
+            Console.WriteLine($" how many sides does your next Die have?");
            
             sides = Convert.ToInt32( Console.ReadLine() );
             Console.WriteLine($" roll a {sides} sided dice ");
@@ -78,16 +86,26 @@ namespace gameProgMethodOverlaodsChrisFrench0259182
         static void RandSidesTimes()
         {
 
-            Console.WriteLine($" how many sides does your Die have?");
+            Console.WriteLine($" how many sides does your next Die have?");
             sides = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($" how many times will you  rool your die?");
-            times = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($" roll a {sides} sided dice ");
-            int roll = random.Next(1, sides + 1);
+            Console.WriteLine($" how many times will you  roll your die?");
+
+            string timesInput = Console.ReadLine();
+            int.TryParse(timesInput, out times);
+
+            for (int i = 0; i < times; i++)
+            {
+                int randomNumber = random.Next(1, sides + 1); // maxValue + 1 to include the maximum value
+                Console.WriteLine($"Random number {i + 1}: {randomNumber}");
+            }
+
+            //times = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($" roll a {sides} sided dice ");
+            //int roll = random.Next(1, sides + 1);
 
 
 
-            Console.WriteLine($" you rolled a {roll}");
+            //Console.WriteLine($" you rolled a {roll}");
             Console.ReadKey(true);
 
 
@@ -97,34 +115,53 @@ namespace gameProgMethodOverlaodsChrisFrench0259182
         //m4
         static void RandRed()
         {
-
-            //Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($" how many sides does your Die have?");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($" how many sides does your next Die have?");
             sides = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($" how many times will you  rool your die?");
-            times = Convert.ToInt32(Console.ReadLine());
-            Console.Write($" roll a ");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write($"{sides} ");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine($"sided dice ");
-            int roll = random.Next(1, sides + 1);
+            Console.WriteLine($" how many times will you  roll your die?");
 
-            Console.Write($" you rolled a ");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write($"{sides} ");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write($"sided dice ");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write($"{times} ");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine($"timmes ");
-            Console.Write($" you rolled a ");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"{roll} ");
-            Console.ForegroundColor = ConsoleColor.White;
+            string timesInput = Console.ReadLine();
+            int.TryParse(timesInput, out times);
 
-        
+            for (int i = 0; i < times; i++)
+            {
+                int randomNumber = random.Next(1, sides + 1); // maxValue + 1 to include the maximum value
+                Console.WriteLine($"Random number {i + 1}: {randomNumber}");
+            }
+
+
+
+
+
+
+
+            //Console.ForegroundColor = ConsoleColor.DarkRed;
+            //Console.WriteLine($" how many sides does your next Die have?");
+            //sides = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($" how many times will you  roll your die?");
+            //times = Convert.ToInt32(Console.ReadLine());
+            //Console.Write($" roll a ");
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.Write($"{sides} ");
+            //Console.ForegroundColor = ConsoleColor.DarkRed;
+            //Console.WriteLine($"sided dice ");
+            //int roll = random.Next(1, sides + 1);
+
+            //Console.Write($" you rolled a ");
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.Write($"{sides} ");
+            //Console.ForegroundColor = ConsoleColor.DarkRed;
+            //Console.Write($"sided dice ");
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.Write($"{times} ");
+            //Console.ForegroundColor = ConsoleColor.DarkRed;
+            //Console.WriteLine($"timmes ");
+            //Console.Write($" you rolled a ");
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine($"{roll} ");
+            //Console.ForegroundColor = ConsoleColor.DarkRed;
+
+
             Console.ReadKey(true);
 
 
