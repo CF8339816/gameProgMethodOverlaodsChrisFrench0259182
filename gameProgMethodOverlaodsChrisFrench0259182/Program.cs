f708f99be61bf742128cd2d4252beca0280c60e2
CF8339816 <chris_french_3dga@hotmail.com>
2025-10-14 21:13:14 -0300

    Update Program.cs

--- gameProgMethodOverlaodsChrisFrench0259182/Program.cs
+++ gameProgMethodOverlaodsChrisFrench0259182/Program.cs
@@ -9,9 +9,6 @@ namespace gameProgMethodOverlaodsChrisFrench0259182
 {
 
 
-    //Random random = new Random();
-    //int randomNumber = random.Next(  ,  );
-
 
 
     internal class Program
@@ -27,8 +24,7 @@ namespace gameProgMethodOverlaodsChrisFrench0259182
 
         static void Main(string[] args)
         {
-            //Random random = new Random();
-            //int roll = random.Next(1, sides + 1 );
+           
 
             SixSide();
             Console.WriteLine();
@@ -41,10 +37,6 @@ namespace gameProgMethodOverlaodsChrisFrench0259182
 
             RandRed();
             Console.WriteLine();
-
-
-
-
 
 
 
@@ -95,17 +87,11 @@ namespace gameProgMethodOverlaodsChrisFrench0259182
 
             for (int i = 0; i < times; i++)
             {
-                int randomNumber = random.Next(1, sides + 1); // maxValue + 1 to include the maximum value
+                int randomNumber = random.Next(1, sides + 1); 
                 Console.WriteLine($"Random number {i + 1}: {randomNumber}");
             }
 
-            //times = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine($" roll a {sides} sided dice ");
-            //int roll = random.Next(1, sides + 1);
-
-
-
-            //Console.WriteLine($" you rolled a {roll}");
+           
             Console.ReadKey(true);
 
 
@@ -115,7 +101,7 @@ namespace gameProgMethodOverlaodsChrisFrench0259182
         //m4
         static void RandRed()
         {
-            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($" how many sides does your next Die have?");
             sides = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($" how many times will you  roll your die?");
@@ -125,42 +111,9 @@ namespace gameProgMethodOverlaodsChrisFrench0259182
 
             for (int i = 0; i < times; i++)
             {
-                int randomNumber = random.Next(1, sides + 1); // maxValue + 1 to include the maximum value
+                int randomNumber = random.Next(1, sides + 1); 
                 Console.WriteLine($"Random number {i + 1}: {randomNumber}");
             }
-
-
-
-
-
-
-
-            //Console.ForegroundColor = ConsoleColor.DarkRed;
-            //Console.WriteLine($" how many sides does your next Die have?");
-            //sides = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine($" how many times will you  roll your die?");
-            //times = Convert.ToInt32(Console.ReadLine());
-            //Console.Write($" roll a ");
-            //Console.ForegroundColor = ConsoleColor.Red;
-            //Console.Write($"{sides} ");
-            //Console.ForegroundColor = ConsoleColor.DarkRed;
-            //Console.WriteLine($"sided dice ");
-            //int roll = random.Next(1, sides + 1);
-
-            //Console.Write($" you rolled a ");
-            //Console.ForegroundColor = ConsoleColor.Red;
-            //Console.Write($"{sides} ");
-            //Console.ForegroundColor = ConsoleColor.DarkRed;
-            //Console.Write($"sided dice ");
-            //Console.ForegroundColor = ConsoleColor.Red;
-            //Console.Write($"{times} ");
-            //Console.ForegroundColor = ConsoleColor.DarkRed;
-            //Console.WriteLine($"timmes ");
-            //Console.Write($" you rolled a ");
-            //Console.ForegroundColor = ConsoleColor.Red;
-            //Console.WriteLine($"{roll} ");
-            //Console.ForegroundColor = ConsoleColor.DarkRed;
-
 
             Console.ReadKey(true);
 
